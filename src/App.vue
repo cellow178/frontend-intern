7d78ce2edcd1ee16424a3d1cd7679577b25d9ec1
cellow178 <samuelmarcelloimmanuel@gmail.com>
2026-07-29 16:18:48 +0700

    Set base CSS and color root; insert school logo; button component;

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import ApiTest from './components/ApiTest.vue'
-
+  import Button from './components/ui/Button.vue';
 </script>
 
 <template>
+  <Button label="Login" :icon="true" />
   <ApiTest />
 </template>
