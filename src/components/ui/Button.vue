d07dc7a11fc707c433cc83e9617318c8dfda5029
cellow178 <samuelmarcelloimmanuel@gmail.com>
2026-07-29 16:25:30 +0700

    Install remixicon

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { RiLoginBoxLine } from '@remixicon/vue'
+
 withDefaults(
   defineProps<{
     label: string
@@ -15,20 +17,6 @@ withDefaults(
     class="flex items-center gap-2 bg-primary hover:bg-primary transition-shadow duration-300 text-neutral font-semibold text-2xl px-8 py-2 rounded-full shadow-lg hover:shadow-[0_4px_20px_#FF9644] cursor-pointer"
   >
     {{ label }}
-    <svg
-      v-if="icon"
-      xmlns="http://www.w3.org/2000/svg"
-      class="w-6 h-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-      />
-    </svg>
+    <RiLoginBoxLine v-if="icon" class="w-6 h-6" />
   </button>
 </template>
